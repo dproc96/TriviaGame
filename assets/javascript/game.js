616054cfbba24b8b89d011253c0e0ab40f24c6c2
Dan Proctor <dproc96@gmail.com>
2019-06-18 16:56:09 -0400

    completed game draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
+    "use strict"
+    var intervalId;
     let gameObject = {
         questions: [],
-        correctAnswers: 0,
-        incorrectAnswers: 0,
+        correctAnswersCount: 0,
+        incorrectAnswersCount: 0,
+        questionIndex: 0,
         generateQuestions() {
             $.ajax({
                 url: `https://opentdb.com/api.php?amount=10&category=20&type=multiple`,
                 method: "GET"
             }).then(function(response) {
-                this.questions = response.results;
+                gameObject.questions = response.results.sort(function() { return 0.5 - Math.random() });
+                console.log(gameObject)
             })
         },
         runTimer(startingTime, afterFunction) {
             $("#main-header").html(startingTime);
             let time = startingTime - 1;
-            let intervalId = setInterval(function () {
+            intervalId = setInterval(function () {
                 if (time === 0) {
                     clearInterval(intervalId);
                     afterFunction();
@@ -25,7 +29,54 @@ $(document).ready(function() {
                     time--;
                 }
             }, 1000)
+        },
+        writeQuestion() {
+            let activeQuestion = this.questions[this.questionIndex];
+            $("#main-text").html(activeQuestion.question);
+            let correctAnswerIndex = Math.floor(Math.random() * 4);
+            let incorrectAnswers = activeQuestion.incorrect_answers.sort(function() { return .5 - Math.random()})
+            let incorrectAnswerIndex = 0;
+            $("#main-buttons").empty();
+            for (let i = 0; i < 4; i++) {
+                if (i === correctAnswerIndex) {
+                    $("#main-buttons").append(`<button class="button-answer" value="true">${activeQuestion.correct_answer}</button>`)
+                }
+                else {
+                    $("#main-buttons").append(`<button class="button-answer" value="false">${incorrectAnswers[incorrectAnswerIndex]}</button>`)                    
+                    incorrectAnswerIndex++
+                }
+            }
+            this.questionIndex++;
+            if (this.questionIndex === 10) {
+                this.generateQuestions()
+                this.questionIndex = 0;
+            }
+            this.runTimer(10, function() { gameObject.evaluateAnswer("false", activeQuestion) })
+            $(".button-answer").click(function() {
+                clearInterval(intervalId);
+                gameObject.evaluateAnswer($(this).attr("value"), activeQuestion);
+            })
+        },
+        evaluateAnswer(answerValue, questionObject) {
+            $("#main-buttons").html("<button id='next'>Next Question?</button>");
+            if (answerValue === "true") {
+                $("#main-text").html("You are correct!")
+                this.correctAnswersCount++
+                $("#correct-count").html(this.correctAnswersCount);
+            }
+            else {
+                $("#main-text").html(`Sorry! The correct answer was "${questionObject.correct_answer}"`)
+                this.incorrectAnswersCount++
+                $("#incorrect-count").html(this.incorrectAnswersCount);
+            }
+            this.runTimer(5, function () { gameObject.writeQuestion() })
+            $("#next").click(function() {
+                clearInterval(intervalId);
+                gameObject.writeQuestion();
+            })
         }
     };
-    gameObject.generateQuestions();   
+    gameObject.generateQuestions();  
+    
+    $("#play").click(function() { gameObject.writeQuestion() });
 })
